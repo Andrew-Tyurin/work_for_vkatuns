9b54d24a0292db6e09d52a8932b652e7142ae057
Andrew-Tyurin <anrej.andrej2017@ya.ru>
2025-09-14 23:51:29 +0300

    Сверстал главную страницу
    Временно использую moc данные

--- main_page_app/views.py
+++ main_page_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from data import *
 
 
 def main_page(request):
-    return render(request, 'main_page_app/index.html', {})
+    return render(request, 'main_page_app/index.html', {'specialties': specialties, 'companies': companies})
