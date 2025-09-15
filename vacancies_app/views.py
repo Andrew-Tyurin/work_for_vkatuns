80ab41e1e1d55bc14a8cc6a81ce5c7701d22374c
Andrew-Tyurin <anrej.andrej2017@ya.ru>
2025-09-16 00:45:03 +0300

    Сверстал страницу для конкретной вакансии(карточка вакансии)
    Временно использую moc данные

--- vacancies_app/views.py
+++ vacancies_app/views.py
@@ -4,12 +4,20 @@ from data import *
 
 
 def vacancies_page(request):
-    jobs_vacancies = jobs
     return render(request, 'vacancies_app/vacancies.html', {'jobs': jobs})
 
 
 def one_vacancy(request, vacancies_id):
-    return render(request, 'vacancies_app/card_one_vacancy.html', {})
+    try:
+        job = jobs[vacancies_id - 1]
+        vacancy_in_the_company = companies[int(job['company']) - 1]
+    except (KeyError, IndexError):
+        raise Http404
+
+    return render(
+        request,
+        'vacancies_app/card_one_vacancy.html',
+        {'job': job, 'company': vacancy_in_the_company})
 
 
 def vacancies_specialty(request, specialty_request):
