c9afa4306ce0713e79c32b2bdf09179e5496d0a2
Andrew-Tyurin <anrej.andrej2017@ya.ru>
2025-11-08 00:54:54 +0300

    тестовый коммит.

--- main/views/views.py
+++ main/views/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponseNotFound, HttpResponseServerError
 
 
 def custom_handler404(request, exception):
-    return HttpResponseNotFound('Ошибка: 404 - страница не найдена')
+    return HttpResponseNotFound('Ошибка: 404 - страница не найдена!')
 
 
 def custom_handler500(request):
-    return HttpResponseServerError('Ошибка: 500 - проблема на сервере')
+    return HttpResponseServerError('Ошибка: 500 - проблема на сервере!')
